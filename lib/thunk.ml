c9f175bfd229c9f2c050182657301ef8e5c89b65
Samuel B. Johnson <sabjohnso@yahoo.com>
2024-05-15 04:47:47 -0500

    :sparkles: Thunk

---
+++ lib/thunk.ml
@@ -0,0 +1,11 @@
+type 'a thunk = unit -> 'a
+
+module Derived_trivial_ =
+  Functor.Trivial_of_minimal_trivial (
+      struct
+        type 'a t = 'a thunk
+        let wrap x = fun () -> x
+        let unwrap mx = mx ()
+      end
+    )
+include Derived_trivial_
---
+++ lib/thunk.mli
@@ -0,0 +1,25 @@
+type 'a thunk = unit -> 'a
+
+val wrap : 'a -> 'a thunk
+val unwrap : 'a thunk -> 'a
+val ( <$> ) : ('a -> 'b) -> 'a thunk -> 'b thunk
+val fapply : ('a -> 'b) thunk -> 'a thunk -> 'b thunk
+val ( <*> ) : ('a -> 'b) thunk -> 'a thunk -> 'b thunk
+type 'a t = 'a thunk
+val fmap : ('a -> 'b) -> 'a thunk -> 'b thunk
+val pure : 'a -> 'a thunk
+val product : 'a thunk -> 'b thunk -> ('a * 'b) thunk
+val bind : 'a thunk -> ('a -> 'b thunk) -> 'b thunk
+val join : 'a thunk thunk -> 'a thunk
+val return : 'a -> 'a thunk
+val flatmap : ('a -> 'b thunk) -> 'a thunk -> 'b thunk
+val ( >>= ) : 'a thunk -> ('a -> 'b thunk) -> 'b thunk
+val ( =<< ) : ('a -> 'b thunk) -> 'a thunk -> 'b thunk
+val ( <=< ) : ('a -> 'b thunk) -> ('c -> 'a thunk) -> 'c -> 'b thunk
+val ( >=> ) : ('a -> 'b thunk) -> ('b -> 'c thunk) -> 'a -> 'c thunk
+val ( let* ) : 'a thunk -> ('a -> 'b thunk) -> 'b thunk
+val ( let+ ) : 'a thunk -> ('a -> 'b) -> 'b thunk
+val ( and+ ) : 'a thunk -> 'b thunk -> ('a * 'b) thunk
+val extract : 'a thunk -> 'a
+val duplicate : 'a -> 'a thunk
+val extend : ('a -> 'b) -> 'a -> 'b thunk
